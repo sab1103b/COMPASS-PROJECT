56dd2f4d94ccb2c97b6b5a9b277d6ef967adc2eb
sab1103b <est.santiagoa.bola1@unimilitar.edu.co>
2025-05-18 21:39:17 -0500

    php

--- api/main.php
+++ api/main.php
@@ -1,14 +1,33 @@
 <?php
 $getJSON = file_get_contents('php://input');
-
 $dataJson = json_decode($getJSON);
 
-// ejemplo de toma de datos del json: 
-$correo = $dataJson -> {'correo'};
-$contrasena = $dataJson -> {'contrasena'};
+// Mostrar todos los datos recibidos del formulario de registro
+echo "<pre>";
+print_r($dataJson);
+echo "</pre>";
 
-echo $getJSON;
-
+// Ejemplo de toma de datos individuales:
+if (isset($dataJson->correo)) {
+    $correo = $dataJson->correo;
+    echo "Correo: $correo<br>";
+}
+if (isset($dataJson->contrasena)) {
+    $contrasena = $dataJson->contrasena;
+    echo "Contraseña: $contrasena<br>";
+}
+if (isset($dataJson->nombre)) {
+    $nombre = $dataJson->nombre;
+    echo "Nombre: $nombre<br>";
+}
+if (isset($dataJson->celular)) {
+    $celular = $dataJson->celular;
+    echo "Celular: $celular<br>";
+}
+if (isset($dataJson->{'verificacion-contrasena'})) {
+    $verif = $dataJson->{'verificacion-contrasena'};
+    echo "Verificación contraseña: $verif<br>";
+}
 
 // base de datos
 $mysqli = new mysqli("localhost", "root", "", "prueba"); // base de datos locales
